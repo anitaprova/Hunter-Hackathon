c99c6e99c97ff23197676623dacedccd5d550cf5
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-05-02 19:56:25 -0400

    added styling for tailwind and navbar

---
+++ my-react-app/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-fushia text-xl flex text-white justify-around p-3 rounded-full ml-25 mr-25 mt-5">
+      <p>Animations</p>
+      <p>Mood Diary</p>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ my-react-app/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-lightpink">
+        <h1 className="text-pink">Calendar</h1>
+
+      </div>
+    </>
+  );
+};
+
+export default Home;
