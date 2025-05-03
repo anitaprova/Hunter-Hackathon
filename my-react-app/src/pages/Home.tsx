ac29ec6b72b2e31bf4e4401cbe3ec36c1704f1f1
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-05-02 20:27:21 -0400

    implemented calendar and styling for it

--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+import "../calendar.css";
+
 
 const Home = () => {
+  const [date, setDate] = useState(new Date());
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleClickDay = (value) => {
+    setDate(value);
+    setShowPopup(true);
+  };
+  
   return (
     <>
-      <div className="bg-lightpink">
-        <h1 className="text-pink">Calendar</h1>
-
+      <div className="bg-white flex flex-col ml-50 mr-50 mt-10 p-5 rounded-3xl">
+        <h1
+          className="text-fushia text-5xl justify-self-center self-center mb-5"
+        >
+          Calendar
+        </h1>
+        <Calendar onClickDay={handleClickDay} />
       </div>
     </>
   );
