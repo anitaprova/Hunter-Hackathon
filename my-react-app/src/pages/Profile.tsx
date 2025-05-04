253276b51c3d34b90b6327398bb39c5e1247a1d9
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-05-03 22:27:47 -0400

    added more customization and simplified logic

--- my-react-app/src/pages/Profile.tsx
+++ my-react-app/src/pages/Profile.tsx
@@ -10,7 +10,25 @@ const Profile = () => {
   const [hair, setHair] = useState<string>("");
   const [eyebrow, setEyebrow] = useState("");
   const [eyes, setEyes] = useState("");
-	const [mouth, setMouth] = useState("");
+  const [mouth, setMouth] = useState("");
+  const [nose, setNose] = useState("");
+  const [skin, setSkin] = useState("base");
+
+  const allHair = [
+    "afro",
+    "bob",
+    "pigtails",
+    "ponytail",
+    "short",
+    "long",
+    "dreads",
+    "hijab",
+  ];
+  const allEyebrows = ["bushy", "normal", "sad"];
+  const allEyes = ["droopy", "excited", "happy", "open", "pretty", "sad"];
+  const allMouths = ["happy", "lipstick", "sad", "smile"];
+  const allNoses = ["round", "shaded", "normal"];
+  const allSkin = ["light", "tan", "medium", "dark"];
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
@@ -19,33 +37,55 @@ const Profile = () => {
   return (
     <>
       <div className="flex gap-x-5 ml-50 mr-50 mt-10 p-5 rounded-3xl text-pink text-2xl mb-15">
-        <div className="bg-white rounded-3xl w-[50%] relative">
-          <img src="src/assets/img/base.png" className="w-full" />{" "}
-          {hair && (
-            <img
-              src={`src/assets/img/hair/${hair}.PNG`}
-              className="absolute top-0 left-0 w-full"
-            />
-          )}
-          {eyebrow && (
-            <img
-              src={`src/assets/img/eyebrows/${eyebrow}.PNG`}
-              className="absolute top-0 left-0 w-full"
-            />
-          )}
-          {eyes && (
-            <img
-              src={`src/assets/img/eyes/${eyes}.PNG`}
-              className="absolute top-0 left-0 w-full"
-            />
-          )}
-          {mouth && (
-            <img
-              src={`src/assets/img/mouth/${mouth}.PNG`}
-              className="absolute top-0 left-0 w-full"
-            />
-          )}
+        <div className="bg-white rounded-3xl w-[50%] ">
+          <div className="relative">
+            <img src={`src/assets/img/skin/${skin}.PNG`} className="w-full" />{" "}
+            {hair && (
+              <img
+                src={`src/assets/img/hair/${hair}.PNG`}
+                className="absolute top-0 left-0 w-full"
+              />
+            )}
+            {eyebrow && (
+              <img
+                src={`src/assets/img/eyebrows/${eyebrow}.PNG`}
+                className="absolute top-0 left-0 w-full"
+              />
+            )}
+            {eyes && (
+              <img
+                src={`src/assets/img/eyes/${eyes}.PNG`}
+                className="absolute top-0 left-0 w-full"
+              />
+            )}
+            {mouth && (
+              <img
+                src={`src/assets/img/mouth/${mouth}.PNG`}
+                className="absolute top-0 left-0 w-full"
+              />
+            )}
+            {nose && (
+              <img
+                src={`src/assets/img/nose/${nose}.PNG`}
+                className="absolute top-0 left-0 w-full"
+              />
+            )}
+          </div>
+          <div className="ml-5 mr-5">
+            <p>Skin colors:</p>
+            <div className="flex gap-x-5 justify-around">
+              {allSkin.map((skin) => (
+                <p
+                  className="cursor-pointer"
+                  onClick={() => setSkin(skin)}
+                >
+                  {skin}
+                </p>
+              ))}
+            </div>
+          </div>
         </div>
+
         <div className="w-[50%]">
           <TabContext value={value}>
             <Box
@@ -59,11 +99,69 @@ const Profile = () => {
                 fontFamily: "Itim",
               }}
             >
-              <TabList onChange={handleChange}>
-                <Tab label="Hair" value="1" />
-                <Tab label="Eyebrows" value="2" />
-                <Tab label="Eyes" value="3" />
-                <Tab label="Mouth" value="4" />
+              <TabList
+                onChange={handleChange}
+                TabIndicatorProps={{
+                  style: {
+                    backgroundColor: "#FF4E50",
+                  },
+                }}
+              >
+                <Tab
+                  label="Hair"
+                  value="1"
+                  sx={{
+                    fontFamily: "Itim",
+                    color: "#FB8DA0",
+                    "&.Mui-selected": {
+                      color: "#FB4570",
+                    },
+                  }}
+                />
+                <Tab
+                  label="Eyebrows"
+                  value="2"
+                  sx={{
+                    fontFamily: "Itim",
+                    color: "#FB8DA0",
+                    "&.Mui-selected": {
+                      color: "#FB4570",
+                    },
+                  }}
+                />
+                <Tab
+                  label="Eyes"
+                  value="3"
+                  sx={{
+                    fontFamily: "Itim",
+                    color: "#FB8DA0",
+                    "&.Mui-selected": {
+                      color: "#FB4570",
+                    },
+                  }}
+                />
+                <Tab
+                  label="Mouth"
+                  value="4"
+                  sx={{
+                    fontFamily: "Itim",
+                    color: "#FB8DA0",
+                    "&.Mui-selected": {
+                      color: "#FB4570",
+                    },
+                  }}
+                />
+                <Tab
+                  label="Nose"
+                  value="5"
+                  sx={{
+                    fontFamily: "Itim",
+                    color: "#FB8DA0",
+                    "&.Mui-selected": {
+                      color: "#FB4570",
+                    },
+                  }}
+                />
               </TabList>
             </Box>
 
@@ -80,100 +178,63 @@ const Profile = () => {
                 width: "full",
               }}
             >
-              <div className="grid grid-cols-2 grid-rows-2">
-                <TabPanel
-                  value="1"
-                  onClick={() => setHair("pigtails")}
-                  className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                >
-                  <img src="src/assets/img/hair/pigtails.PNG" />
-                </TabPanel>
-                <TabPanel
-                  value="1"
-                  onClick={() => setHair("ponytail")}
-                  className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                >
-                  <img src="src/assets/img/hair/ponytail.PNG" />
-                </TabPanel>
-                <TabPanel
-                  value="1"
-                  onClick={() => setHair("short")}
-                  className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                >
-                  <img src="src/assets/img/hair/short.PNG" />
-                </TabPanel>
-                <TabPanel
-                  value="1"
-                  onClick={() => setHair("bob")}
-                  className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                >
-                  <img src="src/assets/img/hair/bob.PNG" />
-                </TabPanel>
+              <div className="grid grid-cols-3 grid-rows-2">
+                {allHair.map((hair) => (
+                  <TabPanel
+                    value="1"
+                    onClick={() => setHair(hair)}
+                    className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
+                  >
+                    <img src={`src/assets/img/hair/${hair}.PNG`} />
+                  </TabPanel>
+                ))}
               </div>
 
               <div className="grid grid-cols-2 ">
-                <TabPanel value="2">
-                  <img
-                    src="src/assets/img/eyebrows/normal.PNG"
-                    onClick={() => setEyebrow("normal")}
+                {allEyebrows.map((eyebrow) => (
+                  <TabPanel
+                    value="2"
+                    onClick={() => setEyebrow(eyebrow)}
                     className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
-                <TabPanel value="2">
-                  <img
-                    src="src/assets/img/eyebrows/bushy.PNG"
-                    onClick={() => setEyebrow("bushy")}
+                  >
+                    <img src={`src/assets/img/eyebrows/${eyebrow}.PNG`} />
+                  </TabPanel>
+                ))}
+              </div>
+
+              <div className="grid grid-cols-3">
+                {allEyes.map((eyes) => (
+                  <TabPanel
+                    value="3"
+                    onClick={() => setEyes(eyes)}
                     className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
-                <TabPanel value="2">
-                  <img
-                    src="src/assets/img/eyebrows/sad.PNG"
-                    onClick={() => setEyebrow("sad")}
-                    className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
+                  >
+                    <img src={`src/assets/img/eyes/${eyes}.PNG`} />
+                  </TabPanel>
+                ))}
               </div>
 
               <div className="grid grid-cols-2 ">
-                <TabPanel value="3">
-                  <img
-                    src="src/assets/img/eyes/excited.PNG"
-                    onClick={() => setEyes("excited")}
+                {allMouths.map((mouths) => (
+                  <TabPanel
+                    value="4"
+                    onClick={() => setMouth(mouths)}
                     className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
-                <TabPanel value="3">
-                  <img
-                    src="src/assets/img/eyes/happy.PNG"
-                    onClick={() => setEyes("happy")}
-                    className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
-                <TabPanel value="3">
-                  <img
-                    src="src/assets/img/eyes/sad.PNG"
-                    onClick={() => setEyes("sad")}
-                    className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
+                  >
+                    <img src={`src/assets/img/mouth/${mouths}.PNG`} />
+                  </TabPanel>
+                ))}
               </div>
-
               <div className="grid grid-cols-2 ">
-                <TabPanel value="4">
-                  <img
-                    src="src/assets/img/mouth/sad.PNG"
-                    onClick={() => setMouth("sad")}
+                {allNoses.map((noses) => (
+                  <TabPanel
+                    value="5"
+                    onClick={() => setNose(noses)}
                     className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
-                <TabPanel value="4">
-                  <img
-                    src="src/assets/img/mouth/happy.PNG"
-                    onClick={() => setMouth("happy")}
-                    className="cursor-pointer hover:border hover:border-solid hover:rounded-3xl hover:border-3"
-                  />
-                </TabPanel>
+                  >
+                    <img src={`src/assets/img/nose/${noses}.PNG`} />
+                  </TabPanel>
+                ))}
               </div>
             </Box>
           </TabContext>
